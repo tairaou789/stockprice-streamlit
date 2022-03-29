c7000506df3e8f6efbe57babb00bbef648b6f14b
tairaou789 <tarareba_iko789@yahoo.co.jp>
2022-03-29 18:32:54 +0900

    3st commit

--- stock_price.py
+++ stock_price.py
@@ -12,45 +12,44 @@ def test():
     ```python
     !pip3 install yfinance -- jupyterで入力
     ```
-
     """
+    st.title('Streamlit 株価チェックアプリ②')
 
-    """
-    ## ★ほかに必要なライブラリ等
-    ```python
-    import pandas as pd
-    import matplotlib.pyplot as plt
-    import yfinance as yf
+    # """
+    # ## ★ほかに必要なライブラリ等
+    # ```python
+    # import pandas as pd
+    # import matplotlib.pyplot as plt
+    # import yfinance as yf
 
-    %matplotlib inline
-    ```
-    """
-    """
-    ### !!ここでエラー発生
-    ```python
-    Duplicate key in file WindowsPath
-    ('C:/anaconda/lib/site-packages/matplotlib/mpl-data/matplotlibrc'), 
-    line 258 ('font.family:  IPAexGothic')
-    ```
-    ### 以下のサイトを参照しました
-    【対策】
-    ①IPAが配布しているIPAexという日本語用フォントをダウンロード
-    ②ipaexg.ttf をmatplotlibのフォント用フォルダに置く
-    ③matplotlibの設定ファイル matplotlibrc でipaexg.ttf を読み込む設定をする
+    # %matplotlib inline
+    # ```
+    # """
+    # """
+    # ### !!ここでエラー発生
+    # ```python
+    # Duplicate key in file WindowsPath
+    # ('C:/anaconda/lib/site-packages/matplotlib/mpl-data/matplotlibrc'), 
+    # line 258 ('font.family:  IPAexGothic')
+    # ```
+    # ### 以下のサイトを参照しました
+    # 【対策】
+    # ①IPAが配布しているIPAexという日本語用フォントをダウンロード
+    # ②ipaexg.ttf をmatplotlibのフォント用フォルダに置く
+    # ③matplotlibの設定ファイル matplotlibrc でipaexg.ttf を読み込む設定をする
 
-    ```python
-    #4-05-1
-    # フォントを格納するフォルダを調べる
+    # ```python
+    # #4-05-1
+    # # フォントを格納するフォルダを調べる
 
-    import matplotlib
-    matplotlib.matplotlib_fname()
+    # import matplotlib
+    # matplotlib.matplotlib_fname()
 
-    下の例では、使用OSは Windows10 で anaconda は D:\\sys\\anaconda3 にインストールされていて、 python仮想環境の名前は book3 である。
+    # 下の例では、使用OSは Windows10 で anaconda は D:\\sys\\anaconda3 にインストールされていて、 python仮想環境の名前は book3 である。
 
-    [matplotlib の設定ファイル]
-        D:\sys\anaconda3\env\book3\Lib\site-packages\matplotlib\mpl-data\matplotlibrc
-    [ttf fontを置くパス]
-        D:\sys\anaconda3\env\book3\Lib\site-packages\matplotlib\mpl-data\fonts\ttf
-    ```
-    """
-    
+    # [matplotlib の設定ファイル]
+    #     D:\sys\anaconda3\env\book3\Lib\site-packages\matplotlib\mpl-data\matplotlibrc
+    # [ttf fontを置くパス]
+    #     D:\sys\anaconda3\env\book3\Lib\site-packages\matplotlib\mpl-data\fonts\ttf
+    # ```
+    # """
